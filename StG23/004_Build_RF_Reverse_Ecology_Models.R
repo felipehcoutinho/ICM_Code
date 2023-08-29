6f288f6bd94c0069f27f7dc99847e31ba54af4e1
felipe <felipehcoutinho@gmail.com>
2023-08-29 18:44:06 +0200

    RF Improvements

--- StG23/004_Build_RF_Reverse_Ecology_Models.R
+++ StG23/004_Build_RF_Reverse_Ecology_Models.R
@@ -1,6 +1,7 @@
 library(ranger)
 library(scales)
 library(Metrics)
+#library(caret)
 options(expressions = 5e5)
 
 args = commandArgs(trailingOnly=TRUE)
@@ -47,9 +48,6 @@ fulldata<-merge(z_response_df,t_z_predictor_df,by="row.names",all.x=TRUE)
 colnames(fulldata)[1]<-"MG_UID"
 fulldata$MG_UID<-as.factor(fulldata$MG_UID)
 rownames(fulldata)<-fulldata$MG_UID
-
-fulldata<-fulldata[complete.cases(fulldata),]
-
 #summary(fulldata[,1:15])
 
 all_rf_stats<-c()
@@ -60,7 +58,7 @@ passed_importance_vals<-c()
 best_rfs<-list()
 
 ###Build RF Models for each response variable
-for (var in response_variables) {
+for (resp_var in response_variables) {
 	#best_prod<-0
 	best_rmse<-+Inf
 	#best_pred<-NA
@@ -77,18 +75,30 @@ for (var in response_variables) {
 	#pass_rmsev<-c()
 	best_stats<-c()
 	
-	Response<-fulldata[[var]]
-	subdata<-subset(fulldata,select=predictor_variables)
+	subdata<-subset(fulldata,select=(c(resp_var,predictor_variables)))
+	subdata<-subdata[complete.cases(subdata),]
+	resp_df<-subdata[[resp_var]]
+	pred_df<-subdata[,c(predictor_variables)]
+	
+	print(paste("Building RF model for Response variable:",resp_var,sep=" "))
+	print("Subset DF dimensions:")
+	print(dim(subdata))
+	#print("Response DF dimensions:")
+	#print(dim(resp_df))
+	#print("Predictor DF dimensions:")
+	#print(dim(pred_df))
 
-	print(paste("Building RF model for Response variable:",var,sep=" "))
+	#trainIndex <- createDataPartition(z_sub_df$Z_Response_Variable, p = 0.8, list = FALSE)
+	#z_train_df<-z_sub_df[trainIndex, ]
+	#z_val_df<-z_sub_df[-trainIndex, ]
 
 	set.seed(3435)	
-	rf_model<-ranger(y=Response, x=subdata, num.trees=tree_num, mtry=var_tries, write.forest=TRUE,probability=FALSE,importance="impurity",min.node.size=node_size,num.threads=threads)
+	rf_model<-ranger(y=resp_df, x=pred_df, num.trees=tree_num, mtry=var_tries, write.forest=TRUE,probability=FALSE,importance="impurity",min.node.size=node_size,num.threads=threads)
 
-	cor_t<-cor.test(rf_model$predictions,Response,method="pearson")
+	cor_t<-cor.test(rf_model$predictions,resp_df,method="pearson")
 
 	#Calculate Root Mean Square Error
-	rmse_t<-rmse(rf_model$predictions,Response)
+	rmse_t<-rmse(rf_model$predictions,resp_df)
 
 	all_cort<-c(all_cort,cor_t$estimate)
 	#all_corv<-c(all_corv,cor_v$estimate)
@@ -98,7 +108,7 @@ for (var in response_variables) {
 		
 	if ((cor_t$estimate < cor_cutoff) || (rmse_t > max_rmse)) {passed<-FALSE } else {passed<-TRUE}
 		
-	all_rf_stats<-rbind(all_rf_stats,c(var,1,cor_t$estimate,rmse_t,passed))
+	all_rf_stats<-rbind(all_rf_stats,c(resp_var,1,cor_t$estimate,rmse_t,passed))
 		
 	#Check if correlation and RMSE values are within allowed cutoff
 	if (passed == FALSE) {
@@ -117,11 +127,12 @@ for (var in response_variables) {
 	passed_importance<-as.data.frame(cbind(names(rf_model$variable.importance),rf_model$variable.importance))
 	colnames(passed_importance)<-c("Predictor","importance")
 	passed_importance$importance<-as.numeric(passed_importance$importance)
-	passed_importance$Response<-var
+	passed_importance$Response<-resp_var
 	#passed_importance$RMSEV<-rmse_t
 	#passed_importance$CorV<-cor_t$estimate
 	#passed_importance$Method<-"Impurity"
-	passed_importance$Normalized_Importance<-passed_importance$importance/max((abs(passed_importance$importance)))
+	#Convert absolute importance to percentages: Note that impurity method only yields positive importance values
+	passed_importance$Relative_Importance<-(passed_importance$importance/sum(passed_importance$importance))*100
 	passed_importance_vals<-rbind(passed_importance_vals,passed_importance)
 
 	#Store best matrix and best ANN based ons RMSE
@@ -133,11 +144,11 @@ for (var in response_variables) {
 		
 	all_best_stats<-rbind(all_best_stats,best_stats)
 	if (is.na(best_RF)[1] == FALSE) {	
-		best_rfs[[var]]<-best_RF
-		pred_data<-as.data.frame(cbind(rf_model$predictions,Response))
+		best_rfs[[resp_var]]<-best_RF
+		pred_data<-as.data.frame(cbind(rf_model$predictions,resp_df))
 		colnames(pred_data)<-c("Predicted","Measured")
-		pred_data$Response_Var<-var
-		pred_data$MG_UID<-as.factor(fulldata$MG_UID)
+		pred_data$Response_Var<-resp_var
+		pred_data$MG_UID<-as.factor(rownames(subdata))
 		pred_data$Error<-sqrt((pred_data$Predicted-pred_data$Measured)**2)
 		all_preds<-rbind(all_preds,subset(pred_data,select=c("Measured","Predicted","Response_Var","MG_UID")))
 	}
@@ -146,7 +157,7 @@ for (var in response_variables) {
 colnames(all_rf_stats)<-c("Response","Iteration","CorV","RMSEv","Passed")
 write.table(all_rf_stats,file="Reverse_Ecology_All_RF_Stats.tsv",sep="\t",append=FALSE,row.names=FALSE,col.names=TRUE,quote=FALSE)
 
-write.table(passed_importance_vals,file="Reverse_Ecology_Passed_RF_Importance.tsv",sep="\t",append=FALSE,row.names=FALSE,col.names=TRUE,quote=FALSE)
+write.table(passed_importance_vals,file="Reverse_Ecology_Best_RF_Importance.tsv",sep="\t",append=FALSE,row.names=FALSE,col.names=TRUE,quote=FALSE)
 
 write.table(all_preds,file="Reverse_Ecology_Best_RF_Predictions.tsv",sep="\t",append=FALSE,row.names=FALSE,col.names=TRUE,quote=FALSE)
 
--- StG23/007_Importance_Analysis_RF_Reverse_Ecology_Models.R
+++ StG23/007_Importance_Analysis_RF_Reverse_Ecology_Models.R
@@ -1,24 +1,79 @@
 library(reshape2)
+library(ggplot2)
+library(RColorBrewer)
+
+phylum_coloring<-c(brewer.pal(9,"Set1"),brewer.pal(4,"Pastel1"),brewer.pal(8,"Dark2")[8],brewer.pal(5,"Set2")[5],brewer.pal(8,"Accent"),brewer.pal(6,"Dark2"))
+names(phylum_coloring)<-c("Actinobacteriota","Alphaproteobacteria","Cyanobacteria","Crenarchaeota","Bacteroidota","Nanoarchaeota","Verrucomicrobiota","Marinisomatota","Myxococcota","Patescibacteria","Gammaproteobacteria","Acidobacteriota","UBP7","Poribacteria","Eremiobacterota","Margulisbacteria","Gemmatimonadota","Desulfobacterota","Nitrospinota","Desulfobacterota_D","Bdellovibrionota","Hydrothermarchaeota","Chlamydiota","SAR324","Halobacteriota","Chloroflexota","Thermoplasmatota","Planctomycetota","Thermoproteota")
 
 args = commandArgs(trailingOnly=TRUE)
 seqinfo_file<-args[1]
 importance_file<-args[2]
+imp_info_file<-args[3]
+group_var<-args[4]
+min_nomr_imp<-as.numeric(args[5])
 
+group_var<-"Host_Phylum_PHIST"
+min_rel_imp<-0.01
+min_rel_imp_sum<-1
 seqinfo_file<-"/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Viruses/Info_Sequences/Full_Info_IMGVR_Scaffolds_Comp_75-100_Max_Conta_0_vOTU_Representatives.tsv"
-importance_file<-"/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Viruses/RE_RF_Models/Reverse_Ecology_Passed_RF_Importance.tsv"
+importance_file<-"/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Viruses/RE_RF_Models/Reverse_Ecology_Best_RF_Importance.tsv"
 
 seqinfo_df<-read.table(file=seqinfo_file,sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE,check.names=FALSE)
-
+summary(seqinfo_df)
 #seqinfo_df$UID<-as.factor(paste(seqinfo_df$UVIG,seqinfo_df$Taxon_oid,seqinfo_df$Scaffold_oid,sep="|"))
 #write.table(seqinfo_df[,c("UID","UVIG","Taxon_oid","Scaffold_oid","Ecosystem.classification","vOTU","Length","Estimated.completeness","Estimated.contamination","Taxonomic.classification","Taxonomic.classification.method","Host.taxonomy.prediction","Host.prediction.method","Host_Domain_IMGVR","Host_Phylum_IMGVR","Host_Class_IMGVR","Host_Order_IMGVR","Host_Family_IMGVR","Host_Genus_IMGVR","Host_Species_IMGVR","MAG","phage_fasta_file","host_fasta_file","X.common.kmers","pvalue","adj.pvalue","Host_Domain_PHIST","Host_Phylum_PHIST","Host_Class_PHIST","Host_Order_PHIST","Host_Family_PHIST","Host_Genus_PHIST","Host_Species_PHIST")],file=seqinfo_file,sep="\t",append=FALSE,row.names=FALSE,col.names=TRUE,quote=FALSE)
-
-summary(seqinfo_df)
+#seqinfo_df<-read.table(file=seqinfo_file,sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE,check.names=FALSE)
 
 imp_df<-read.table(file=importance_file,sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE,check.names=FALSE)
 summary(imp_df)
 
-norm_imp_df<-dcast(imp_df,Predictor~Response,value.var="Normalized_Importance",fill=NA)
+imp_df<-merge(imp_df,seqinfo_df[,c("UID",group_var)],by.x="Predictor",by.y="UID",all.x=TRUE)
+
+#Plot Importance Sums barplots
+sum_imp_df<-aggregate(imp_df$Relative_Importance,by=list(Response=imp_df$Response,Group=imp_df[[group_var]]),FUN=sum)
+colnames(sum_imp_df)[3]<-"Relative_Importance_Sum"
+summary(sum_imp_df)
+
+f_sum_imp_df<-sum_imp_df[which(sum_imp_df$Relative_Importance_Sum >= min_rel_imp_sum),]
+
+figX<-ggplot(data=f_sum_imp_df,aes(x=Response, y=Relative_Importance_Sum, fill=Group))+
+geom_bar(position="stack",stat="identity",colour="black",alpha=0.9)+ #,group=f_sum_imp_df[[group_var]]
+scale_fill_manual(name="Predicted Host Taxon",values=phylum_coloring)+
+theme_bw()+
+theme(legend.position="top", axis.text.x = element_text(angle = 45,hjust = 1,size=5))+ #, strip.text.y.right = element_text(angle = 0, size =9),legend.title=element_text(size=9)
+#facet_grid(. ~ Response,scales="free_x")+
+#guides(col = guide_legend(nrow = 1))+
+xlab(NULL)+
+ylab("Sum of Relative Relative Importance by Predictor Group")
+
+ggsave("/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Viruses/RE_RF_Models/Reverse_Ecology_Best_RF_Relative_Importance_Sum_by_Phylum_Barplots.pdf",plot=figX,device=cairo_pdf,width=10,height=7,pointsize=8)
+
+#Build dataframe of relative importances by scaffold UID
+norm_imp_df<-dcast(imp_df,Predictor~Response,value.var="Relative_Importance",fill=NA)
 summary(norm_imp_df)
 
 norm_imp_df<-merge(norm_imp_df,seqinfo_df,by.x="Predictor",by.y="UID",all.x=TRUE)
 summary(norm_imp_df)
+
+write.table(norm_imp_df,file="/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Viruses/RE_RF_Models/Reverse_Ecology_Best_RF_Relative_Importance+Seq_info.tsv",sep="\t",append=FALSE,row.names=FALSE,col.names=TRUE,quote=FALSE)
+
+#Plot Filtered Importance boxplots
+f_imp_df<-imp_df[which(imp_df$Relative_Importance >= min_rel_imp),]
+
+#Replace NA is f_imp_df[[group_var]] with "Unknown"
+clean_names<-as.vector(f_imp_df[[group_var]])
+clean_names[is.na(clean_names)]<-"Unknown"
+f_imp_df[[group_var]]<-as.factor(clean_names)
+summary(f_imp_df)
+
+figX<-ggplot()+
+geom_boxplot(data=f_imp_df,aes(x=f_imp_df[[group_var]], y=log10(Relative_Importance),fill=f_imp_df[[group_var]],group=f_imp_df[[group_var]]),size=0.3,alpha=0.9)+
+theme_bw()+
+scale_fill_manual(name="Predicted Host Taxon",values=phylum_coloring)+
+theme(legend.position="top", axis.text.x = element_text(angle = 90,hjust = 1,size=7), strip.text.y.right = element_text(angle = 0, size =9),legend.title=element_text(size=9))+
+facet_grid(. ~ Response,scales="free_x")+
+guides(col = guide_legend(nrow = 1))+
+xlab(NULL)+
+ylab("Log10(Relative Predictor Importance)")
+
+ggsave("/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Viruses/RE_RF_Models/Reverse_Ecology_Best_RF_Relative_Importance_by_Phylum_Boxplots.pdf",plot=figX,device=cairo_pdf,width=22,height=6,pointsize=8)
