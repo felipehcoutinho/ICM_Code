874368a229d7247574a6c9f703ad1bfa36cfab4b
felipe <felipehcoutinho@gmail.com>
2023-11-24 15:50:09 +0100

    ANN and RF Updates

--- EcoModels.py
+++ EcoModels.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
 from collections import defaultdict
 import scipy.stats
+import pickle
 #from scipy.stats import zscore
 import pandas as pd
 import numpy as np
@@ -35,144 +36,121 @@ parser.add_argument("--z_transform", help="Flag to Z-transform values of predict
 parser.add_argument("--threads", help="Number of threads to be used during analysis", default=1, type = int)
 parser.add_argument("--build_ann", help="Flag to run the ANN module", default=False, type = bool)
 parser.add_argument("--build_rf", help="Flag to run the Random Forest module", default=False, type = bool)
-parser.add_argument("--rf_trees", help="Number of trees to grow in each RF", default=100, type = int)
 parser.add_argument("--min_pred_prev", help="Minimum number of non-zero values required for variables in the preditor DF to be include in models", default=1, type = int)
 parser.add_argument("--min_resp_prev", help="Minimum number of non-zero values required for variables in the response DF to be include in models", default=1, type = int)
-parser.add_argument("--k_cv", help="Value of k t be used for k-fold cross validations. Skipped if k = 0", default=0, type = int)
+parser.add_argument("--run_hpt", help="Flag to run hyperparameter tuning with grid search cross validation to find best HP to be used for training a final model", default=False, type = bool)
+parser.add_argument("--hpt_rf_trees", help="Number of trees to grow in each RF", default=[100,500,1000], type = int, nargs="+")
+parser.add_argument("--hpt_k_cv", help="Value of k t be used for k-fold cross validations durign hyperparameter tuning. Skipped if k = 0", default=5, type = int)
+parser.add_argument("--hpt_ann_n_neurons", help="Values of number of neurons in the hidden layer to be evaluated during ANN hyperparameter tuning", default=[2,3,5,10], type = int,  nargs="+")
+parser.add_argument("--hpt_ann_learning_rate_init", help="Values of number of initial learning rate to be evaluated during ANN hyperparameter tuning", default=[0.001, 0.005, 0.01, 0.05], type = float,  nargs="+")
+parser.add_argument("--hpt_ann_max_iter", help="Values of maximum number of iterations to be evaluated during ANN hyperparameter tuning", default=[50,100,500], type = int,  nargs="+")
 args = parser.parse_args()
 
 
 def central():
-    #Format the datasets fto be used by the models
+    #Format the datasets to be used by the models any columns with NA values in any samples are removed)
     (full_df,pred_df,resp_df,valid_pred_var_names,valid_resp_var_names) = format_datasets()
     #Build ANN models
     if (args.build_ann == True):
-        #Format the datasets for ANN models (i.e. any columns with NA values in any samples are removed)
-        full_MOR = build_ann(pred_df=pred_df,resp_df=resp_df)
-        full_perfo_info_df = calc_performance_metrics(mor_model=full_MOR, pred_df=pred_df,resp_df=resp_df, print_preds=True)
-        full_perfo_info_df["Dataset"] = "Full"
-        output_dataframe_file = args.prefix + "_Full_Models_Performance_Info.tsv"
-        full_perfo_info_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+        full_MOR = build_model(pred_df=pred_df,resp_df=resp_df,model_type="ANN")
+        #full_MOR = build_ann(pred_df=pred_df,resp_df=resp_df)
+        (performance_df, predictions_df) = calc_performance_metrics(mor_model=full_MOR, pred_df=pred_df,resp_df=resp_df, model_type="ANN", dataset="Full", print_perfo=True, print_preds=True)
     #Build RF models
     if (args.build_rf == True):
-        rf_full_df = full_df
-        #Run k-fold cross validation if specified
-        if (args.k_cv > 0):
-            print(f"Running {args.k_cv}-fold cross validation")
-            kf = KFold(n_splits=args.k_cv,shuffle=True,random_state=42)
-            fold_idx = 0
-            perfo_dfs = []
-            for train_index, test_index in kf.split(rf_full_df):
-                fold_idx = fold_idx + 1
-                print(f"Running fold {fold_idx}")
-                #Split into training and test sets
-                full_cv_train_df = rf_full_df.iloc[train_index]
-                full_cv_test_df = rf_full_df.iloc[test_index]
-                #Split into predictor and response variables
-                cv_train_pred_df = full_cv_train_df[valid_pred_var_names]
-                cv_train_resp_df = full_cv_train_df[valid_resp_var_names]
-                cv_MOR = build_rf(pred_df=cv_train_pred_df,resp_df=cv_train_resp_df,rf_trees=args.rf_trees)
-                #Calculate performance metrics of the CV models on Training set
-                cv_train_perfo_info_df = calc_performance_metrics(mor_model=cv_MOR, pred_df=cv_train_pred_df,resp_df=cv_train_resp_df)
-                cv_train_perfo_info_df["Fold"] = fold_idx
-                cv_train_perfo_info_df["Dataset"] = "Training"
-                #Calculate performance metrics of the CV models on Test set
-                cv_test_pred_df = full_cv_test_df[valid_pred_var_names]
-                cv_test_resp_df = full_cv_test_df[valid_resp_var_names]
-                cv_test_perfo_info_df = calc_performance_metrics(mor_model=cv_MOR, pred_df=cv_test_pred_df,resp_df=cv_test_resp_df)
-                cv_test_perfo_info_df["Fold"] = fold_idx
-                cv_test_perfo_info_df["Dataset"] = "Test"
-                #Merge the two dataframes
-                cv_perfo_info_df = pd.concat([cv_train_perfo_info_df, cv_test_perfo_info_df], axis=0)
-                perfo_dfs.append(cv_perfo_info_df)
-            #Merge the performance dataframes from each fold
-            full_cv_perfo_info_df = pd.concat(perfo_dfs, axis=0)
-            output_dataframe_file = args.prefix + "_CV_Models_Performance_Info.tsv"
-            full_cv_perfo_info_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
         #Build a model using the full dataset
-        full_MOR = build_rf(pred_df=rf_full_df[valid_pred_var_names],resp_df=rf_full_df[valid_resp_var_names],rf_trees=args.rf_trees)
-        full_perfo_info_df = calc_performance_metrics(mor_model=full_MOR, pred_df=rf_full_df[valid_pred_var_names],resp_df=rf_full_df[valid_resp_var_names], print_preds=True)
-        full_perfo_info_df["Dataset"] = "Full"
-        output_dataframe_file = args.prefix + "_Full_Models_Performance_Info.tsv"
-        full_perfo_info_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+        full_MOR = build_model(pred_df=pred_df,resp_df=resp_df,model_type="RF")
+        (performance_df, predictions_df) = calc_performance_metrics(mor_model=full_MOR, pred_df=pred_df,resp_df=resp_df, model_type="RF", dataset="Full", print_perfo=True, print_preds=True)
 
-def format_datasets():
-    if args.resp_pred_table:
-        #Warn user if the resp_pred_file is provide dby the predictor and response variables are empty, then die
-        if (args.predictor_vars == None) or (args.response_vars == None):
-            print("WARNING: When porividing the resp_pred_file both predictor AND response variables must be specified")
-            exit()
-        #For simplicity, the predictor and response variables are loaded and processed separately then merged
-        (resp_df, valid_resp_var_names) = prepare_single_df(type="response",df_file=args.resp_pred_table,min_var_prev=args.min_resp_prev,idx_var=args.response_index,transpose=args.transpose_response,z_transform=args.z_transform,valid_var_names=args.response_vars)
-        (pred_df, valid_pred_var_names) = prepare_single_df(type="predictor",df_file=args.resp_pred_table,min_var_prev=args.min_pred_prev,idx_var=args.predictor_index,transpose=args.transpose_predictors,z_transform=args.z_transform,valid_var_names=args.predictor_vars)
-    else:
-        (resp_df, valid_resp_var_names) = prepare_single_df(type="response",df_file=args.response_table,min_var_prev=args.min_resp_prev,idx_var=args.response_index,transpose=args.transpose_response,z_transform=args.z_transform,valid_var_names=args.response_vars)
-        (pred_df, valid_pred_var_names) = prepare_single_df(type="predictor",df_file=args.predictor_table,min_var_prev=args.min_pred_prev,idx_var=args.predictor_index,transpose=args.transpose_predictors,z_transform=args.z_transform,valid_var_names=args.predictor_vars)
-    
-    full_df = pd.concat([resp_df, pred_df], axis=1, join='inner')
-    print(f"Shape of full DF: {full_df.shape}")
-    output_dataframe_file = args.prefix + "_Full_Processed_Data_Frame.tsv"
-    full_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
-    pred_df = full_df[valid_pred_var_names]
-    resp_df = full_df[valid_resp_var_names]
-    return(full_df,pred_df,resp_df,valid_pred_var_names,valid_resp_var_names)
-
-@ignore_warnings(category=ConvergenceWarning)
-def build_ann(resp_df=None, pred_df=None):
-    #Perform hyper parameter tuning using grid search
-    param_grid={'estimator__learning_rate_init': [0.001, 0.005, 0.01], 'estimator__max_iter': [100], 'estimator__hidden_layer_sizes': [2,3,5]}
-    hpt_ann = MLPRegressor(random_state=42)
-    hpt_MOR = MultiOutputRegressor(hpt_ann,n_jobs=1)
-    print(f"Performing ANN hyperparameter tuning")
-    gsMOR = GridSearchCV(estimator=hpt_MOR, param_grid=param_grid, refit=True, cv=KFold(n_splits=5), verbose=3, n_jobs=args.threads, return_train_score=True, scoring="r2") #cv=5, scoring="r2"neg_mean_squared_error
-    gsMOR.fit(pred_df, resp_df)
-    print(f"Best ANN hyperparameters: {gsMOR.best_params_}")
-    
-    hpt_results_df = pd.DataFrame(gsMOR.cv_results_)
-    output_dataframe_file = args.prefix + "_ANN_Hyper_Parameter_Tuning_Metrics.tsv"
-    hpt_results_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
-    #Buiild a model for each response variable using the MultiOutputRegressor function
-    #print(f"Training ANN models")
-    #Set up ANN. Hyperparams are the same for all the output variables
-    #ann = MLPRegressor(hidden_layer_sizes=10, max_iter=1000, learning_rate_init=0.01, random_state=42)
-    # define the direct multioutput MOR model
-    #MOR = MultiOutputRegressor(ann,n_jobs=args.threads)
-    #MOR.fit(pred_df, resp_df)
-    #Calculate performance metrics of the models
-    best_MOR = gsMOR.best_estimator_
-    #Print the number of hidden layers in the first object in best_MOR
-    print(f"Total Number of layers (input + hidden + output) in best Model: {best_MOR.estimators_[0].n_layers_}")
-    return(best_MOR)
-
-def calc_performance_metrics(mor_model=None, pred_df=None, resp_df=None, print_preds=False):
+def calc_performance_metrics(mor_model=None, pred_df=None, resp_df=None, model_type=None, dataset="NA", print_perfo=False, print_preds=False):
     print("Calculating model performance metrics")
     model_info = defaultdict(dict)
+    #Generarte predictions for the response variables based on the output of the received model
     predictions = mor_model.predict(pred_df)
     resp_var_names = resp_df.columns
     predictions_df = pd.DataFrame(predictions, columns=resp_var_names, index=resp_df.index)
     predictions_df.index.name = resp_df.index.name
-    #predictions_df["Sample"] = pred_df.index
-    # print(f"Summary of model predictions:")
-    # print(predictions_df.describe())
-    #predictions_df.index.name = 'Response_Variable'
-    #print(predictions_df.describe())
+    #Calculate model performance metrics for each response variable in the MOR model
     for rvar in resp_var_names:
         model_info[rvar]["PearsonR"] = scipy.stats.pearsonr(predictions_df[rvar], resp_df[rvar])[0]
         model_info[rvar]["RMSE"] = np.sqrt(np.mean((predictions_df[rvar] - resp_df[rvar].values.ravel())**2))
         #print(f"Response Variable: {rvar}, PearsonR: {model_info[rvar]['PearsonR']}, RMSE: {model_info[rvar]['RMSE']}")
-    model_info_df = pd.DataFrame.from_dict(model_info)
-    model_info_df = model_info_df.transpose()
-    model_info_df.index.name = 'Response_Variable'
-
-    if print_preds == True:
-        #Merge the predictions with the true values of the response variables in resp_df indicating which is each
-        predictions_df["Data_Type"] = "Predicted"
-        resp_df["Data_Type"] = "Measured"
-        predictions_df = pd.concat([predictions_df, resp_df], axis=0)
-        output_dataframe_file = args.prefix + "_Full_Models_Predicted_vs_Measured_Values.tsv"
+    performance_df = pd.DataFrame.from_dict(model_info)
+    performance_df = performance_df.transpose()
+    performance_df.index.name = 'Response_Variable'
+    if (print_perfo == True):
+        performance_df["Dataset"] = dataset
+        output_dataframe_file = args.prefix + model_type + dataset + "_Models_Performance_Info.tsv"
+        performance_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+    
+    #Merge the predictions with the expected/true values of the response variables in resp_df indicating which is each. This must be done after the performance metrics are calculated so that the DF maintain the correc dimensions
+    predictions_df["Data_Type"] = "Predicted"
+    resp_df["Data_Type"] = "Measured"
+    predictions_df = pd.concat([predictions_df, resp_df], axis=0)
+    if (print_preds == True):
+        output_dataframe_file = args.prefix + model_type + dataset  + "_Models_Predicted_vs_Measured_Values.tsv"
         predictions_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
     
-    return(model_info_df)
+    return(performance_df,predictions_df)
+
+def build_model(resp_df=None, pred_df=None, model_type=None):
+    #Instantiate a model deppending on the type of model to be built
+    if (model_type == "ANN"):
+        #Set up an ANN model with default hyperparamters= {'learning_rate_init': 0.001, 'max_iter': 100, 'hidden_layer_sizes': (2,)}
+        model = MLPRegressor(random_state=42,learning_rate_init=0.01, max_iter=100, hidden_layer_sizes=(5,))
+        param_grid={'estimator__learning_rate_init': args.hpt_ann_learning_rate_init, 'estimator__max_iter': args.hpt_ann_max_iter, 'estimator__hidden_layer_sizes': args.hpt_ann_n_neurons}
+    elif (model_type == "RF"):
+        model = RandomForestRegressor(random_state=42, n_jobs=1, n_estimators=100)
+        param_grid={'estimator__n_estimators': args.hpt_rf_trees}
+    #Instantiate a MultiOutputRegressor object to build a model for each response variable
+    MOR = MultiOutputRegressor(model,n_jobs=1)
+    #Instantiate a GridSearchCV object to perform hyperparameter tuning
+    if (args.run_hpt == True):
+        #Set combinations of hyperparameters to be tested
+        HPT = GridSearchCV(estimator=MOR, param_grid=param_grid, refit=True, cv=KFold(n_splits=args.hpt_k_cv, shuffle=True), verbose=0, n_jobs=args.threads, return_train_score=True, scoring="neg_mean_squared_error")
+        #Perform the grid search, obtain best parameters, and fit the final model
+        HPT.fit(pred_df, resp_df)
+        hpt_results_df = pd.DataFrame(HPT.cv_results_)
+        output_dataframe_file = args.prefix + model_type +"_Hyper_Parameter_Tuning_Metrics.tsv"
+        hpt_results_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+        best_MOR = HPT.best_estimator_
+    else:
+        #Fit the single model
+        MOR.fit(pred_df, resp_df)
+        best_MOR = MOR
+    #Save model to file with pickle.dump
+    output_model_file = args.prefix + "_" + model_type +"_MOR_Model.pkl"
+    pickle.dump(best_MOR, open(output_model_file, 'wb'))
+    return(best_MOR)
+
+def build_ann(resp_df=None, pred_df=None):
+    #Instantiate ANN
+    ann = MLPRegressor(random_state=42)
+    #Buiild a model for each response variable using the MultiOutputRegressor function
+    MOR = MultiOutputRegressor(ann,n_jobs=1)
+    print(f"Performing ANN hyperparameter tuning")
+    #Set combinations of hyperparameters to be tested
+    param_grid={'estimator__learning_rate_init': args.hpt_ann_learning_rate_init, 'estimator__max_iter': args.hpt_ann_max_iter, 'estimator__hidden_layer_sizes': args.hpt_ann_n_neurons}
+    #Set up the grid search
+    HPT = GridSearchCV(estimator=MOR, param_grid=param_grid, refit=True, cv=KFold(n_splits=args.hpt_k_cv, shuffle=True), verbose=0, n_jobs=args.threads, return_train_score=True, scoring="neg_mean_squared_error") #cv=5, scoring="r2"neg_mean_squared_error
+    #Perform the grid search, obtain best parameters, and fit the final model
+    HPT.fit(pred_df, resp_df)
+    #print(f"Best ANN hyperparameters: {HPT.best_params_}")
+    #The best score is actually defined as the mean_test_score over all folds. This is the score that is used to select the best parameters before refitting a final
+    #print(f"Best Score: {HPT.best_score_}")
+    #Builda DF from the results of the grid search and print it to a file
+    hpt_results_df = pd.DataFrame(HPT.cv_results_)
+    output_dataframe_file = args.prefix + "_ANN_Hyper_Parameter_Tuning_Metrics.tsv"
+    hpt_results_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+
+    #Obtain the best models from the grid search after refitting
+    best_MOR = HPT.best_estimator_
+    #Print the number of hidden layers in the first object in best_MOR
+    #print(f"Total Number of layers (input + hidden + output) in best Model: {best_MOR.estimators_[0].n_layers_}")
+    #Save model to file with pickle.dump
+    output_model_file = args.prefix + "_Best_ANN_MOR_Model.pkl"
+    pickle.dump(best_MOR, open(output_model_file, 'wb'))
+
+    return(best_MOR)
 
 def build_rf(pred_df=None, resp_df=None, rf_trees=100):
     """Build a Random Forest model for each response variable according to the user specifications"""
@@ -202,7 +180,40 @@ def build_rf(pred_df=None, resp_df=None, rf_trees=100):
     full_imp_df = pd.concat(imp_dfs_list,axis=1)
     output_dataframe_file = args.prefix + "_Predictor_Importance.tsv"
     #full_imp_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+    #Save model to file with pickle.dump
+    output_model_file = args.prefix + "_Best_RF_MOR_Model.pkl"
+    pickle.dump(MOR, open(output_model_file, 'wb'))
     return(MOR)
+    
+def format_datasets():
+    if args.resp_pred_table:
+        #Warn user if the resp_pred_file is provide dby the predictor and response variables are empty, then die
+        if (args.predictor_vars == None) or (args.response_vars == None):
+            print("WARNING: When porividing the resp_pred_file both predictor AND response variables must be specified")
+            exit()
+        #For simplicity, the predictor and response variables are loaded and processed separately then merged
+        resp_file = args.resp_pred_table
+        pred_file = args.resp_pred_table
+    else:
+        resp_file = args.response_table
+        pred_file = args.predictor_table
+    #Load and process the predictor and response variables
+    (resp_df, valid_resp_var_names) = prepare_single_df(type="response",df_file=resp_file,min_var_prev=args.min_resp_prev,idx_var=args.response_index,transpose=args.transpose_response,z_transform=args.z_transform,valid_var_names=args.response_vars)
+    (pred_df, valid_pred_var_names) = prepare_single_df(type="predictor",df_file=pred_file,min_var_prev=args.min_pred_prev,idx_var=args.predictor_index,transpose=args.transpose_predictors,z_transform=args.z_transform,valid_var_names=args.predictor_vars)
+    
+    full_df = pd.concat([resp_df, pred_df], axis=1, join='inner')
+    print(f"Shape of full DF: {full_df.shape}")
+    output_dataframe_file = args.prefix + "_Full_Processed_Data_Frame.tsv"
+    full_df.to_csv(output_dataframe_file,sep="\t",na_rep='NA')
+    pred_df = full_df[valid_pred_var_names]
+    resp_df = full_df[valid_resp_var_names]
+    return(full_df,pred_df,resp_df,valid_pred_var_names,valid_resp_var_names)
+
+
+
+
+
+
 
 
 def prepare_single_df(type=None,df_file=None,min_var_prev=1,idx_var=None,transpose=False,z_transform=False,valid_var_names=None):
--- StG23/001_StG23_Make_TARA_Euk_ASV_Abund_Table.R
+++ StG23/001_StG23_Make_TARA_Euk_ASV_Abund_Table.R
@@ -1,9 +1,19 @@
 library(reshape2)
 
-metadata<-read.table(file="/mnt/lustre/scratch/fcoutinho/StG_23/Metadata/TARA_MGs_Paired_With_Delmont_Data_Info+Metadata.tsv",sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE)
+metadata<-read.table(file="/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Metadata/TARA_MGs_Paired_With_Delmont_Data_Info+Metadata.tsv",sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE)
 
-asvdata<-read.table(file="/mnt/lustre/scratch/fcoutinho/StG_23/Eukaryotes/Abundance/metapr2_ASVs_selected_abundance_Eukaryota_2023-07-01.tsv",sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE)
+prjdata1<-read.table(file="/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Eukaryotes/Abundance/filereport_read_run_PRJEB6610.tsv",sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE)
+
+prjdata1<-read.table(file="/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Eukaryotes/Abundance/filereport_read_run_PRJEB4352.tsv",sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE)
+
+asvdata<-read.table(file="/mnt/lustre/scratch/nlsas/home/csic/eyg/fhc/StG23/Eukaryotes/Abundance/metapr2_ASVs_selected_abundance_Eukaryota_2023-07-01.tsv",sep="\t",header=TRUE,quote="",comment="",stringsAsFactors=TRUE)
 
 summary(asvdata)
 
-abdata<-dcast(asvdata,asv_code~file_code,value.var="n_reads",fill=0)
+abdata<-dcast(asvdata,file_code~asv_code,value.var="n_reads_pct",fill=0)
+
+summary(abdata[,1:5])
+
+abdata<-merge(abdata,metadata,by.x="file_code",by.y="ENA_Run_ID",all.x=TRUE)
+
+summary(abdata)
